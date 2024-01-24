bcdebdfacef6c178722140c407a0295a1a264f28
Jiahao Li <jiahaoli0465@gmail.com>
2024-01-24 17:26:31 -0500

    added more base code for app.js

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express');
+const ExpressError = require("./expressError")
+
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+
+
+app.use(function(err, req, res, next) {
+    // the default status is 500 Internal Server Error
+    let status = err.status || 500;
+    let message = err.message;
+  
+    // set the status and alert the user
+    return res.status(status).json({
+      error: {message, status}
+    });
+  });
+
+app.listen(3000, function() {
+    console.log('Server is listening on port 3000');
+});
