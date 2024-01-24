1271a2e88d549d119928a0d945aac39108e83bc8
Jiahao Li <jiahaoli0465@gmail.com>
2024-01-24 17:51:39 -0500

    implemented calculateMedian

--- helperFunctions.js
+++ helperFunctions.js
@@ -4,6 +4,17 @@ function calculateMean(nums) {
 }
 
 function calculateMedian(nums) {
+    nums.sort((a, b) => a - b);
+
+    const midIndex = Math.floor(nums.length / 2);
+
+    // If the array has an odd length, return the middle element
+    if (nums.length % 2 !== 0) {
+        return nums[midIndex];
+    }
+
+    // If the array has an even length, return the average of the two middle elements
+    return (nums[midIndex - 1] + nums[midIndex]) / 2;
 
 }
 
@@ -14,4 +25,4 @@ function numToArray(arr) {
     });
 }
 
-module.exports = { calculateMean, numToArray };
+module.exports = { calculateMean, numToArray, calculateMedian };
